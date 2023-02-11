c2e5e29beb4ff16d49f4ee4e1dc4eaa608ca0613
divertente <57573562+Analyzings@users.noreply.github.com>
2023-02-11 23:02:32 +0100

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,18 +1,16 @@
-
 import random
 import sys
 
 chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!?$%&"
 
 while 1:
-    password_len = int(input("What lenght would you like your pass to be : "))
-    password_count = int(input("How many passwords would you like (max is 1 for now): "))
-    for x in range(0,password_count):
-        password = ""
-        for x in range(0,password_len):
-            password_char = random.choice(chars)
-            password      = password + password_char
-            with open('log.txt', 'w') as sys.stdout:
-                print("Here's your pass : ", password)
-
-  
+password_len = int(input("What lenght would you like your pass to be : "))
+password_count = int(input("How many passwords would you like (max is 1 for now): "))
+for x in range(0,password_count):
+password = ""
+for x in range(0,password_len):
+password_char = random.choice(chars)
+password = password + password_char
+with open('logs.txt', 'a') as file:
+file.write("Here's your pass : " + password + "\n")
+print("Here's your pass : ", password)
